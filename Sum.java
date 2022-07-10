e3d312ab612705cb1c32fb0181b22e193019c937
Jatin <jatinsaini40@gmail.com>
2022-07-10 17:17:20 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,14 @@
+public class Sum{
+
+           public static void public static void main(String[] args) {
+            
+
+            int a = 7;
+            int b = 8;
+
+            System.out.println(a+b);
+
+           }
+            
+        
+}
